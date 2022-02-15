ef94588de5b6ac2568ff07618e17517f4e933c5e
İbrahim Yılgör <yilgor16@itu.edu.tr>
2022-02-15 19:46:35 +0300

    feat: about page design

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from "react";
 import './About.css';
+import logo from '../logo.png'; // with import
 
 const About = () => {
     
@@ -9,7 +10,48 @@ const About = () => {
 
   return (
     <div className="About">
-      <h2>HAKKIMIZDA</h2>
+      <div className="PhotoAndText">
+        <div className="Top">
+          <div className="Photo">
+            <img width="100%" alt="logo" height="100%" src={logo} />
+          </div>
+          <div className="Title">
+            <p>John Doe</p>
+            <p>Lawyer</p>
+          </div>
+        </div>
+        <div className="Bottom">
+          <p>Detail</p>
+        </div>
+      </div>
+      <div className="PhotoAndText">
+        <div className="Top">
+          <div className="Photo">
+            <img width="100%" alt="logo" height="100%" src={logo} />
+          </div>
+          <div className="Title">
+            <p>John Doe</p>
+            <p>Lawyer</p>
+          </div>
+        </div>
+        <div className="Bottom">
+          <p>Detail</p>
+        </div>
+      </div>
+      <div className="PhotoAndText">
+        <div className="Top">
+          <div className="Photo">
+            <img width="100%" alt="logo" height="100%" src={logo} />
+          </div>
+          <div className="Title">
+            <p>John Doe</p>
+            <p>Lawyer</p>
+          </div>
+        </div>
+        <div className="Bottom">
+          <p>Detail</p>
+        </div>
+      </div>
     </div>
     
   );
