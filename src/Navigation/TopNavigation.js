a9077a84922ab5dfc58f1902f127c3990f73a8bf
İbrahim Yılgör <yilgor16@itu.edu.tr>
2022-04-22 00:27:39 +0300

    logo fit its div and new nav for mobile

--- src/Navigation/TopNavigation.js
+++ src/Navigation/TopNavigation.js
@@ -3,10 +3,30 @@ import './TopNav.css';
 import logo from '../logo.png'; // with import
 import logo2 from '../logo2.png'; // with import
 import {NavLink, useLocation} from "react-router-dom";
+import ListIcon from '@mui/icons-material/List';
+import useWindowDimensions from '../getDimensions';
+import { IconButton, Popover, Typography } from '@mui/material';
 
 const TopNavigation = () => {
+  const dim = useWindowDimensions();
+
   const location = useLocation();
   const [selected,setSelected] = useState(0);
+
+  const [aboutItems, setAboutItems] = useState(false);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   useEffect(() => {
     console.log(location.pathname);
@@ -34,18 +54,55 @@ const TopNavigation = () => {
           </div>
         </div>
         <div className="RightTopNav">
-          <div className="NavAndBox">
-            <NavLink to="/" className={selected===1 ? "SelectedLink" : "Link"} >Ana Sayfa</NavLink>
-            <div className={selected===1 ? "Highlight" : "NoHighlight"}></div>
-          </div>
-          <div className="NavAndBox">
-            <NavLink to="/hakkimizda" className={selected===2 ? "SelectedLink" : "Link"}>Hakkımızda</NavLink>
-            <div className={selected===2 ? "Highlight" : "NoHighlight"}></div>
-          </div>
-          <div className="NavAndBox">
-            <NavLink to="/iletisim" className={selected===3 ? "SelectedLink" : "Link"}>İletişim</NavLink>
-            <div className={selected===3 ? "Highlight" : "NoHighlight"}></div>
-          </div>
+          {dim.width > dim.height && (<><div className="NavAndBox">
+            <NavLink to="/" className={selected === 1 ? "SelectedLink" : "Link"}>Ana Sayfa</NavLink>
+            <div className={selected === 1 ? "Highlight" : "NoHighlight"}></div>
+          </div><div className="NavAndBox">
+              <NavLink to="/hakkimizda" className={selected === 2 ? "SelectedLink" : "Link"}>Hakkımızda</NavLink>
+              <div className={selected === 2 ? "Highlight" : "NoHighlight"}></div>
+            </div><div className="NavAndBox">
+              <NavLink to="/iletisim" className={selected === 3 ? "SelectedLink" : "Link"}>İletişim</NavLink>
+              <div className={selected === 3 ? "Highlight" : "NoHighlight"}></div>
+            </div></>)}
+          {dim.width <= dim.height && (
+            <IconButton style={{maxWidth: 'auto', maxHeight: 'auto', minWidth: 'auto', minHeight: 'auto'}} onClick={handleClick} /* className={classes.iconbutton} */ aria-label="delete">
+              <ListIcon fontSize="large" />
+          </IconButton>
+          )}
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            onClick={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+            <>
+              <div className="PopoverItem">
+              <NavLink to="/" className={selected === 1 ? "SelectedLink" : "Link"}>Ana Sayfa</NavLink>
+              </div>
+              <div className="PopoverItem">
+              <NavLink to="/hakkimizda" className={selected === 2 ? "SelectedLink" : "Link"}>Hakkımızda</NavLink>
+              </div>
+              {/* {aboutItems && (
+                <><div className="PopoverItem">
+                  <NavLink to="/" className={selected === 1 ? "SelectedLink" : "Link"}>Ana Sayfa</NavLink>
+                </div><div className="PopoverItem">
+                    <NavLink to="/hakkimizda" className={selected === 2 ? "SelectedLink" : "Link"}>Hakkımızda</NavLink>
+                  </div></>)
+              } */}
+              <div className="PopoverItem">
+              <NavLink to="/iletisim" className={selected === 3 ? "SelectedLink" : "Link"}>İletişim</NavLink>
+              </div>
+           </>
+          </Popover>
         </div>
       </div>
       <div className="Line">
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -6,6 +6,8 @@ import useWindowSize from "../useWindowSize";
 import EmailIcon from '@mui/icons-material/Email';
 import MapIcon from '@mui/icons-material/Map';
 import CallIcon from '@mui/icons-material/Call';
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 const Users = ({ 
   google, 
@@ -28,17 +30,33 @@ const Users = ({
         <div className="Text">
           <div className="IconAndText">
             <div className="Icon">
-              <MapIcon sx={{ fontSize: 80 }}></MapIcon>
+              <MapIcon sx={{ height: "10vh", width: "10vh"}}></MapIcon>
             </div>
-            <h3 className="P">
-              Kavaklıdere Mahallesi Bestekar Caddesi Sadibey Apartmanı No:40/2 Çankaya/Ankara
-            </h3>
+            <Box sx={{ height: "100%", display: "flex", justifyContent: "space-between", flexDirection: "column"}}>
+                <Grid xs={12} >
+                  <Typography sx={{fontSize: "2.5vh", textAlign:"center", height: "100%", display: "grid", alignItems: "center"}}>Kavaklıdere Mahallesi Bestekar Caddesi Sadibey Apartmanı No: 40/2 Çankaya/Ankara</Typography>
+                </Grid>
+              </Box>
           </div>
           <div className="IconAndText">
             <div className="Icon">
-              <CallIcon sx={{ fontSize: 80 }}></CallIcon>
+              <CallIcon sx={{ height: "10vh", width: "10vh"}}></CallIcon>
               </div>
-              <h3 className="P2">
+              <Box sx={{ height: "100%", display: "flex", justifyContent: "space-between", flexDirection: "column"}}>
+                <Grid xs={12} >
+                  <Typography sx={{textDecoration: "underline", fontSize: "2.5vh", textAlign:"center",  height: "100%", display: "grid", alignItems: "center"}}>{"Av. Ömer Alperen Balay"}</Typography>
+                </Grid>
+                <Grid xs={12}>
+                  <Typography sx={{fontSize: "2.5vh", textAlign:"center",  height: "100%"}}>{"+90 532 013 43 29"}</Typography>
+                </Grid>
+                <Grid xs={12}>
+                  <Typography sx={{textDecoration: "underline", fontSize: "2.5vh", textAlign:"center",  height: "100%", display: "grid", alignItems: "center"}}>{"Av. Bahadır Dirim"}</Typography>
+                </Grid>
+                <Grid xs={12}>
+                  <Typography sx={{fontSize: "2.5vh", textAlign:"center",  height: "100%"}}>{"+90 551 599 02 24"}</Typography>
+                </Grid>
+              </Box>
+             {/*  <h3 className="P2">
                 {"Av. Ömer Alperen Balay"}
               </h3>
               <h3 className="P3">
@@ -49,15 +67,29 @@ const Users = ({
               </h3>
               <h3 className="P3">
                 {"+90 551 599 02 24"}
-              </h3>
-            </div>
+              </h3>*/}
+            </div> 
           
           
           <div className="IconAndText">
           <div className="Icon">
-            <EmailIcon sx={{ fontSize: 80 }} ></EmailIcon>
+            <EmailIcon sx={{ height: "10vh", width: "10vh"}} ></EmailIcon>
             </div>
-            <h3 className="P2">
+            <Box sx={{ height: "100%", display: "flex", justifyContent: "space-between", flexDirection: "column"}}>
+                <Grid xs={12} >
+                  <Typography sx={{textDecoration: "underline", fontSize: "2.5vh", textAlign:"center", height: "100%", display: "grid", alignItems: "center"}}>{"Av. Ömer Alperen Balay"}</Typography>
+                </Grid>
+                <Grid xs={12}>
+                  <Typography sx={{fontSize: "2.5vh", textAlign:"center", height: "100%"}}> {"omer.alperen@icloud.com"}</Typography>
+                </Grid>
+                <Grid xs={12}>
+                  <Typography sx={{textDecoration: "underline", fontSize: "2.5vh", textAlign:"center", height: "100%", display: "grid", alignItems: "center"}}>{"Av. Bahadır Dirim"}</Typography>
+                </Grid>
+                <Grid xs={12}>
+                  <Typography sx={{fontSize: "2.5vh", textAlign:"center", height: "100%"}}>{"av.bahadir.dirim@gmail.com"}</Typography>
+                </Grid>
+              </Box>
+            {/* <h3 className="P2">
               {"Av. Ömer Alperen Balay"}
             </h3>
             <h3 className="P2">
@@ -68,7 +100,7 @@ const Users = ({
             </h3>
             <h3 className="P2">
               {"av.bahadir.dirim@gmail.com"}
-            </h3>
+            </h3> */}
           </div> 
         </div>
       </div>
